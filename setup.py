8b806056276f0c08e35bdd01d7dcf8257ceb9c24
Henri Blancke <blanckehenri@gmail.com>
2021-10-13 13:46:14 -0400

    NO-REF Update dbt to 0.21.* (#1)

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ except (IOError, ImportError):
     long_description = "Python dbt executor with custom logging and alerting that fits into your stack"
 
 INSTALL_REQUIRES = [
-    'dbt-core==0.19.*', 'datadog==0.40.1', 'python-dotenv==0.17.0',
-    'pygments>=2.4.0', 'sentry-sdk==1.1.0', "prometheus-client==0.11.0"
+    'dbt-core==0.21.*', 'datadog==0.40.1', 'python-dotenv==0.17.0',
+    'pygments>=2.4.0', 'sentry-sdk==1.4.*', "prometheus-client==0.11.0"
 ]
 
 TEST_REQUIRES = [
@@ -33,7 +33,7 @@ TEST_REQUIRES = [
 
 setup(
     name='pydbt',
-    version="0.5.2",
+    version="0.5.3",
     url='https://github.com/henriblancke/dbt-py',
     license='MIT',
     author="Henri Blancke",
