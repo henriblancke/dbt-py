7c7efbdbf923dad8dcd0b2f4f479d2dd8b16b8f4
Henri Blancke <blanckehenri@gmail.com>
2021-10-13 13:15:05 -0400

    [UPD] bump version to 0.5.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ TEST_REQUIRES = [
 
 setup(
     name='pydbt',
-    version="0.5.2",
+    version="0.5.3",
     url='https://github.com/henriblancke/dbt-py',
     license='MIT',
     author="Henri Blancke",
